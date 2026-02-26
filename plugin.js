bb99fe7426450bdf6f2d8382541e9836a24fec78
putilovgr <putilovgr@intellect-soft.ru>
2026-02-26 20:32:29 +0400

    fix visibility

--- plugin.js
+++ plugin.js
@@ -3,39 +3,44 @@
 
     function startPlugin() {
         window.plugin_mybutton_ready = true;
-        console.log('plugin started');
-        // HTML шаблон кнопки
-        var button = `
-            <div class="full-start__button view--mybutton" style="cursor: pointer;">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z"/>
-                </svg>
-                <span>Моя кнопка</span>
-            </div>
-        `;
+
+        var button_html = '\
+            <div class="full-start__button selector view--mybutton">\
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">\
+                    <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z"/>\
+                </svg>\
+                <span>Моя кнопка</span>\
+            </div>\
+        ';
 
         function add() {
-            // Подписываемся на событие открытия страницы фильма
             Lampa.Listener.follow('full', function (e) {
                 if (e.type == 'complite') {
                     try {
-                        var btn = $(button);
+                        var btn = $(button_html);
 
-                        // Обработчик нажатия кнопки
                         btn.on('hover:enter', function () {
-                            var card = e.data.movie || e.data;
-                            Lampa.Noty.show('Нажата кнопка для: ' + (card.title || card.name));
+                            var movie = e.data.movie || e.data;
+                            Lampa.Noty.show('Нажата кнопка для: ' + (movie.title || movie.name));
                         });
 
-                        // Вставляем кнопку после последней кнопки в панели
-                        if (e.data && e.object) {
-                            e.object.activity.render()
-                                .find('.full-start__button')
-                                .last()
-                                .after(btn);
+                        // Ключевое отличие: правильный способ получить render()
+                        var render = e.object.activity.render();
+                        var buttons = render.find('.full-start__buttons');
+
+                        if (buttons.length) {
+                            // Если есть контейнер кнопок — добавляем в него
+                            buttons.append(btn);
+                        } else {
+                            // Fallback: ищем любую кнопку и вставляем после неё
+                            render.find('.full-start__button').last().after(btn);
                         }
+
+                        // Регистрируем кнопку в навигации Lampa
+                        Lampa.Controller.enable('full');
+
                     } catch (error) {
-                        console.error('[mybutton] Ошибка при добавлении кнопки:', error);
+                        console.error('[mybutton]', error);
                     }
                 }
             });
